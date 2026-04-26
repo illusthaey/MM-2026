67289b3a103b778945d7e0dfefc33bac7a9014fc
illusthaey <edusproutcomics@naver.com>
2026-04-26 16:14:33 +0900

    Delete static/retro-footer.js

--- static/retro-footer.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  if (window.__STANDARD_BRAND_FOOTER_LOADED__) return;
-  const script = document.createElement("script");
-  script.src = "/static/footer.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
